8825784c726f2f6b24bb7c0eca6bfcb683182c4d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-02-11 14:56:16 +0100

    All tests green

--- src/main/java/shouty/Person.java
+++ src/main/java/shouty/Person.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Person {
     private final Postbud postbud;
     private List<String> mottatteMeldinger = new ArrayList<String>();
+    private double[] geoLoc;
 
     public Person(Postbud postbud) {
         this.postbud = postbud;
@@ -13,7 +14,7 @@ public class Person {
     }
 
     public void erPå(double[] geoLoc) {
-
+        this.geoLoc = geoLoc;
     }
 
     public void roper(String beskjed) {
@@ -26,5 +27,13 @@ public class Person {
 
     public void hør(String beskjed) {
         mottatteMeldinger.add(beskjed);
+    }
+
+    public double getLat() {
+        return geoLoc[0];
+    }
+
+    public double getLon() {
+        return geoLoc[1];
     }
 }
--- src/main/java/shouty/Postbud.java
+++ src/main/java/shouty/Postbud.java
@@ -8,29 +8,34 @@ public class Postbud {
 
     public void distribuer(Person avsender, String beskjed) {
         for (Person abonnent : abonnenter) {
-            if(erInneforRekkevidde(avsender, abonnent)) {
+            if (erInneforRekkevidde(avsender, abonnent)) {
                 abonnent.hør(beskjed);
             }
         }
     }
 
     private boolean erInneforRekkevidde(Person avsender, Person mottaker) {
-        return true;
+        return distFrom(
+                avsender.getLat(),
+                avsender.getLon(),
+                mottaker.getLat(),
+                mottaker.getLon()
+        ) <= 1;
     }
 
     public void abonner(Person abonnent) {
         abonnenter.add(abonnent);
     }
 
-    private float distFrom(float lat1, float lng1, float lat2, float lng2) {
+    private double distFrom(double lat1, double lng1, double lat2, double lng2) {
         double earthRadius = 6371; //kilometers
-        double dLat = Math.toRadians(lat2-lat1);
-        double dLng = Math.toRadians(lng2-lng1);
-        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLng = Math.toRadians(lng2 - lng1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
                 Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
-                        Math.sin(dLng/2) * Math.sin(dLng/2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        float dist = (float) (earthRadius * c);
+                        Math.sin(dLng / 2) * Math.sin(dLng / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double dist = earthRadius * c;
 
         return dist;
     }
