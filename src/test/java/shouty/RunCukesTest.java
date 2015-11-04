cb6473e231a4ad37f2eae43aaf06d142ecc6427b
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-11-04 15:20:09 +0000

    Move config from class to props. Use colours.

--- src/test/java/shouty/RunCukesTest.java
+++ src/test/java/shouty/RunCukesTest.java
@@ -1,10 +1,8 @@
 package shouty;
 
-import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(monochrome = true, plugin = {"pretty", "html:target/cucumber", "rerun:target/rerun.txt"})
 public class RunCukesTest {
 }
