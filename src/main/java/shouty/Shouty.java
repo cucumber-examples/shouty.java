478a55f7a4bf999a148e4edf5a5efbcae63fceb2
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-11-04 15:31:11 +0000

    Second passing step. Still no implementation.

--- src/main/java/shouty/Shouty.java
+++ src/main/java/shouty/Shouty.java
@@ -3,4 +3,7 @@ package shouty;
 public class Shouty {
     public void setLocation(String personName, int location) {
     }
+
+    public void shout(String personName, String message) {
+    }
 }
--- src/test/java/shouty/ShoutSteps.java
+++ src/test/java/shouty/ShoutSteps.java
@@ -1,6 +1,7 @@
 package shouty;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
 
 public class ShoutSteps {
     private final Shouty shouty = new Shouty();
@@ -9,6 +10,11 @@ public class ShoutSteps {
     public void lucyIsMFromSean(int distance) throws Throwable {
         shouty.setLocation("Sean", 0);
         shouty.setLocation("Lucy", distance);
+    }
+
+    @When("^Sean shouts$")
+    public void seanShouts() throws Throwable {
+        shouty.shout("Sean", "arbitrary message");
     }
 
 }
