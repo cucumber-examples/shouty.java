0486aee3bd9c5cbd862fb9937157d5baa1566e30
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-11-04 15:25:08 +0000

    First failing step - compile error

--- src/test/java/shouty/ShoutSteps.java
+++ src/test/java/shouty/ShoutSteps.java
@@ -1,5 +1,14 @@
 package shouty;
 
+import cucumber.api.java.en.Given;
+
 public class ShoutSteps {
+    private final Shouty shouty = new Shouty();
+
+    @Given("^Lucy is (\\d+)m from Sean$")
+    public void lucyIsMFromSean(int distance) throws Throwable {
+        shouty.setLocation("Sean", 0);
+        shouty.setLocation("Lucy", distance);
+    }
 
 }
