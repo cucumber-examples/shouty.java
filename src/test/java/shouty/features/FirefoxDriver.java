de3273e4f3a25675830290ebe9f8cbbc9e825285
Matt Wynne <matt@mattwynne.net>
2015-11-12 09:18:28 +1100

    Tidy up

---
+++ src/test/java/shouty/features/FirefoxDriver.java
@@ -0,0 +1,3 @@
+package shouty.features;
+
+public class FirefoxDriver extends org.openqa.selenium.firefox.FirefoxDriver { }
--- src/test/java/shouty/features/WebShoutSupport.java
+++ src/test/java/shouty/features/WebShoutSupport.java
@@ -5,7 +5,6 @@ import cucumber.api.java.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import shouty.web.ShoutyServer;
 
 import java.util.HashMap;
