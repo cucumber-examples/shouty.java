7d9f85eade734ac8a6728b25fd0847495c4aeb6e
Tom Stuart <tom@codon.com>
2016-01-04 18:14:36 +0000

    Make second step pass

--- src/main/java/shouty/Shouty.java
+++ src/main/java/shouty/Shouty.java
@@ -3,4 +3,7 @@ package shouty;
 public class Shouty {
     public void setLocation(String personName, int locationInMetres) {
     }
+
+    public void shout(String personName, String message) {
+    }
 }
