18bab78b7bc30d32884f50c8164f4fe37dd9a742
Tom Stuart <tom@codon.com>
2016-01-04 17:54:21 +0000

    Add pending steps for first scenario

--- src/test/java/shouty/ShoutSteps.java
+++ src/test/java/shouty/ShoutSteps.java
@@ -1,5 +1,26 @@
 package shouty;
 
-public class ShoutSteps {
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
+public class ShoutSteps {
+    @Given("^Lucy is (\\d+)m from Sean$")
+    public void lucy_is_m_from_Sean(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^Sean shouts$")
+    public void sean_shouts() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Lucy should hear nothing$")
+    public void lucy_should_hear_nothing() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
 }
