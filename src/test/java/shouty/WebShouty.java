19431ebb0e71dd95807c85fafdcd0cacb80d99bf
Tom Stuart <tom@codon.com>
2016-01-04 22:09:15 +0000

    Make real PUT request with Unirest

--- src/test/java/shouty/WebShouty.java
+++ src/test/java/shouty/WebShouty.java
@@ -1,11 +1,40 @@
 package shouty;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import shouty.web.ShoutyWebServer;
+
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 public class WebShouty implements Shouty {
+    private final ShoutyWebServer server;
+
+    public WebShouty() {
+        server = new ShoutyWebServer(8080);
+
+        try {
+            server.start();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public void setLocation(String personName, int locationInMetres) {
-        throw new UnsupportedOperationException();
+        HttpResponse<String> response;
+
+        try {
+            response = Unirest.put("http://localhost:8080/people/" + personName + "/location")
+                    .body(String.valueOf(locationInMetres))
+                    .asString();
+        } catch (UnirestException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(200, response.getStatus());
     }
 
     @Override
