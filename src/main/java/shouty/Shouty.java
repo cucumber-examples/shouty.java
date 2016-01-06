9219ff53dda2d7d0789f4a9db3c45f8489d0058f
mrorat <michal.rorat@gmail.com>
2016-01-06 13:40:51 +0000

    Shouty method implementations

--- src/main/java/shouty/Shouty.java
+++ src/main/java/shouty/Shouty.java
@@ -1,16 +1,37 @@
 package shouty;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Shouty {
+
+    Map<String, Integer> locations = new HashMap<String, Integer>();
+    Map<String, List<String>> shouts = new HashMap<String, List<String>>();
+
     public void setLocation(String personName, int location) {
+        locations.put(personName, location);
+    }
+
+    public int getDistance(String personName1, String personName2)
+    {
+        if (!locations.containsKey(personName1) || !locations.containsKey(personName2))
+            return Integer.MAX_VALUE;
+
+        return Math.abs(locations.get(personName1) - locations.get(personName2));
     }
 
     public void shout(String personName, String message) {
+        if (shouts.containsKey(personName))
+        {
+            List<String> personShouts = shouts.get(personName);
+            personShouts.add(message);
+            shouts.put(personName, personShouts);
+        }
     }
 
     public List<String> getMessagesHeardBy(String personName) {
-        return new ArrayList();
+        if (shouts.containsKey(personName))
+            return shouts.get(personName);
+        else
+            return Collections.emptyList();
     }
 }
