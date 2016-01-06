59c346cd38889e3f7513b78bbf818cb461e79139
Tom Stuart <tom@codon.com>
2016-01-06 11:34:40 +0000

    Make first step pass

--- src/test/java/shouty/ShoutSteps.java
+++ src/test/java/shouty/ShoutSteps.java
@@ -8,8 +8,9 @@ import cucumber.api.java.en.When;
 public class ShoutSteps {
     @Given("^Linda is (\\d+)m away from Fred$")
     public void linda_is_m_away_from_Fred(int distanceInMetres) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        Shouty shouty = new Shouty();
+        shouty.setLocation("Linda", 0);
+        shouty.setLocation("Fred", distanceInMetres);
     }
 
     @When("^Fred shouts$")
