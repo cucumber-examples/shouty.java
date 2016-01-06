0ba043e8aa238a74738246b3d09108a4b4cfa112
Tom Stuart <tom@codon.com>
2016-01-06 13:57:00 +0000

    Make second scenario pass

---
+++ src/main/java/shouty/Person.java
@@ -0,0 +1,21 @@
+package shouty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    private int location;
+    private List<String> messagesHeard = new ArrayList();
+
+    public void setLocation(int location) {
+        this.location = location;
+    }
+
+    public void hear(String message) {
+        messagesHeard.add(message);
+    }
+
+    public List<String> getMessagesHeard() {
+        return messagesHeard;
+    }
+}
--- src/main/java/shouty/Shouty.java
+++ src/main/java/shouty/Shouty.java
@@ -1,16 +1,36 @@
 package shouty;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Shouty {
+    private Map<String, Person> people = new HashMap();
+
     public void setLocation(String personName, int location) {
+        getPersonCalled(personName).setLocation(location);
+    }
+
+    private Person getPersonCalled(String personName) {
+        Person person = people.get(personName);
+
+        if (person == null) {
+            person = new Person();
+            people.put(personName, person);
+        }
+
+        return person;
     }
 
     public void shout(String personName, String message) {
+        for (Person person : people.values()) {
+            person.hear(message);
+        }
     }
 
     public List<String> getMessagesHeardBy(String personName) {
-        return new ArrayList();
+        Person person = getPersonCalled(personName);
+        return person.getMessagesHeard();
     }
 }
