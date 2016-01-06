4f38d82415675453e710dbc09aa531845bcac3ee
Tom Stuart <tom@codon.com>
2016-01-06 15:51:01 +0000

    Make real POST and GET requests with Selenium

--- src/test/java/shouty/WebShouty.java
+++ src/test/java/shouty/WebShouty.java
@@ -3,15 +3,21 @@ package shouty;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import shouty.web.ShoutyWebServer;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
 import static org.junit.Assert.assertEquals;
 
 public class WebShouty implements Shouty {
 
     private final ShoutyWebServer server;
+    private final WebDriver browser = new FirefoxDriver();
 
     public WebShouty() {
         server = new ShoutyWebServer(8080);
@@ -40,16 +46,20 @@ public class WebShouty implements Shouty {
 
     @Override
     public void shout(String personName, String message) {
-        // HTTP GET request to http://localhost:8080/people/personName
-        // fill in message box with message
-        // Submit form
+        browser.get("http://localhost:8080/people/" + personName);
+        WebElement messageField = browser.findElement(By.name("message"));
+        messageField.sendKeys(message);
+        messageField.submit();
     }
 
     @Override
     public List<String> getMessagesHeardBy(String personName) {
-        // HTTP GET request to http://localhost:8080/people/personName
-        // find all messages on the page
-        // return the messages
-        return null;
+        browser.get("http://localhost:8080/people/" + personName);
+        List<WebElement> messageElements = browser.findElements(By.cssSelector(".messages li"));
+
+        List<String> messages =
+                messageElements.stream().map(WebElement::getText).collect(toList());
+
+        return messages;
     }
 }
