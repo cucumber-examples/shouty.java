c51cd63123ff8d9c6db313ba16490a1a78dfab31
Tom Stuart <tom@codon.com>
2016-01-06 11:40:10 +0000

    Make second step pass

--- src/test/java/shouty/ShoutSteps.java
+++ src/test/java/shouty/ShoutSteps.java
@@ -6,17 +6,18 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class ShoutSteps {
+
+    private final Shouty shouty = new Shouty();
+
     @Given("^Linda is (\\d+)m away from Fred$")
     public void linda_is_m_away_from_Fred(int distanceInMetres) throws Throwable {
-        Shouty shouty = new Shouty();
         shouty.setLocation("Linda", 0);
         shouty.setLocation("Fred", distanceInMetres);
     }
 
     @When("^Fred shouts$")
     public void fred_shouts() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        shouty.shout("Fred", "Free bagels!");
     }
 
     @Then("^Linda should hear nothing$")
