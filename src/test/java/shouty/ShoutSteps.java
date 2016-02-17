312289744920868a338f46cc8bfaeb56b6196619
Steve Tooke <steve@boxjump.co.uk>
2016-02-17 09:33:28 +0000

    DataTables!

--- src/test/java/shouty/ShoutSteps.java
+++ src/test/java/shouty/ShoutSteps.java
@@ -1,5 +1,34 @@
 package shouty;
 
-public class ShoutSteps {
+import java.util.List;
+import cucumber.api.DataTable;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
+public class ShoutSteps {
+	private List<ShoppingListItem> shoppingList;
+
+	@Given("^a shopping list:$")
+	public void i_have_lots_of_data(List<ShoppingListItem> shoppingList) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+		this.shoppingList = shoppingList;
+	}
+	
+	@When("^I go shopping$")
+	public void i_go_shopping() throws Throwable {
+	}
+
+	@Then("^I should have the following items in my cupboard:$")
+	public void i_should_have_the_following_items_in_my_cupboard(DataTable cupboardItems) throws Throwable {
+		cupboardItems.diff(shoppingList);
+	}
+	
+	public static class ShoppingListItem {
+		public String item;
+		public int amount;
+	}
 }
--- src/test/resources/shouty/hear_shout.feature
+++ src/test/resources/shouty/hear_shout.feature
@@ -1,5 +1,16 @@
 Feature: Hear Shout
-
+  
   A description goes here...
 
-  Scenario: ????
+  Scenario: Getting started
+    Given a shopping list:
+      | item      | amount |
+      | apples    | 3      |
+      | cucumbers | 99     |
+      | steak     | 5      |
+    When I go shopping
+    Then I should have the following items in my cupboard:
+      | item      | amount |
+      | apples    | 3      |
+      | cucumbers | 9      |
+      | steak     | 5      |
