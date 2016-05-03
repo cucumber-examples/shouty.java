6b747f2d8eb3e3072afb692a6e48e57328976a08
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2016-05-03 08:13:42 -0400

    Fix formatting. 4 space indent for Java

--- src/main/java/shouty/Shouty.java
+++ src/main/java/shouty/Shouty.java
@@ -1,6 +1,7 @@
 package shouty;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Shouty {
     private static final int MESSAGE_RANGE = 1000;
@@ -8,17 +9,17 @@ public class Shouty {
     private Map<String, String> messages = new HashMap<String, String>();
 
     public void setLocation(String person, Coordinate location) {
-      locations.put(person, location);
+        locations.put(person, location);
     }
 
     public void shout(String person, String message) {
-      messages.put(person, message);
+        messages.put(person, message);
     }
 
     public Map<String, String> getMessagesHeardBy(String listener) {
         HashMap<String, String> result = new HashMap<String, String>();
 
-        for (Map.Entry<String, String> entry: messages.entrySet()) {
+        for (Map.Entry<String, String> entry : messages.entrySet()) {
             String shouter = entry.getKey();
             String message = entry.getValue();
             int distance = locations.get(listener).distanceFrom(locations.get(shouter));
--- src/test/java/shouty/ShoutSteps.java
+++ src/test/java/shouty/ShoutSteps.java
@@ -1,36 +1,37 @@
 package shouty;
 
-import cucumber.api.java.en.*;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import org.junit.Assert;
 
 
 public class ShoutSteps {
-  private final String ARBITRARY_MESSAGE = "Hello, world";
-  private final Shouty shouty = new Shouty();
+    private final String ARBITRARY_MESSAGE = "Hello, world";
+    private final Shouty shouty = new Shouty();
+    
+    @Given("^Lucy is at \\[(\\d+), (\\d+)\\]$")
+    public void lucy_is_at(int xCoord, int yCoord) throws Throwable {
+        shouty.setLocation("Lucy", new Coordinate(xCoord, yCoord));
+    }
 
+    @Given("^Sean is at \\[(\\d+), (\\d+)\\]$")
+    public void sean_is_at(int xCoord, int yCoord) throws Throwable {
+        shouty.setLocation("Sean", new Coordinate(xCoord, yCoord));
+    }
 
-  @Given("^Lucy is at \\[(\\d+), (\\d+)\\]$")
-  public void lucy_is_at(int xCoord, int yCoord) throws Throwable {
-    shouty.setLocation("Lucy", new Coordinate(xCoord, yCoord));
-  }
+    @When("^Sean shouts$")
+    public void sean_shouts() throws Throwable {
+        shouty.shout("Sean", ARBITRARY_MESSAGE);
+    }
 
-  @Given("^Sean is at \\[(\\d+), (\\d+)\\]$")
-  public void sean_is_at(int xCoord, int yCoord) throws Throwable {
-    shouty.setLocation("Sean", new Coordinate(xCoord, yCoord));
-  }
+    @Then("^Lucy should hear Sean")
+    public void lucy_should_hear_sean() throws Throwable {
+        Assert.assertEquals(1, shouty.getMessagesHeardBy("Lucy").size());
+    }
 
-  @When("^Sean shouts$")
-  public void sean_shouts() throws Throwable {
-      shouty.shout("Sean", ARBITRARY_MESSAGE);
-  }
-
-  @Then("^Lucy should hear Sean")
-  public void lucy_should_hear_sean() throws Throwable {
-      Assert.assertEquals(1, shouty.getMessagesHeardBy("Lucy").size());
-  }
-
-  @Then("^Lucy should hear nothing$")
-  public void lucy_should_hear_nothing() throws Throwable {
-      Assert.assertEquals(0, shouty.getMessagesHeardBy("Lucy").size());
-  }
+    @Then("^Lucy should hear nothing$")
+    public void lucy_should_hear_nothing() throws Throwable {
+        Assert.assertEquals(0, shouty.getMessagesHeardBy("Lucy").size());
+    }
 }
