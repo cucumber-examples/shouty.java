06f7689a37dc5b3e55061a0aa46a8d5192f24b25
Aslak Hellesøy <aslakhellesoy@users.noreply.github.com>
2016-09-05 23:19:29 +0200

    More idiomatic junit test

--- src/test/java/shouty/CoordinateTest.java
+++ src/test/java/shouty/CoordinateTest.java
@@ -1,12 +1,12 @@
 package shouty;
 
-import junit.framework.TestCase;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-public class CoordinateTest extends TestCase {
+public class CoordinateTest {
 
     @Test
-    public void testItCalculatesTheDistanceFromAnotherCoordinateAlongXAxis() {
+    public void itCalculatesTheDistanceFromAnotherCoordinateAlongXAxis() {
         Coordinate a = new Coordinate(0, 0);
         Coordinate b = new Coordinate(1000, 0);
         assertEquals(1000, a.distanceFrom(b));
