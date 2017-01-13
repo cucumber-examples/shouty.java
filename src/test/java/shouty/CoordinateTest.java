9fb72cec39281a42de7eb0ecd1b441c22e917abf
Seb Rose <github@claysnow.co.uk>
2017-01-13 13:27:50 +0000

    Update CoordinateTest.java

--- src/test/java/shouty/CoordinateTest.java
+++ src/test/java/shouty/CoordinateTest.java
@@ -5,6 +5,13 @@ import static org.junit.Assert.assertEquals;
 
 public class CoordinateTest {
 
+
+    @Test
+    public void itCalculatesTheDistanceFromItself() {
+        Coordinate a = new Coordinate(0, 0);
+        assertEquals(0, a.distanceFrom(a));
+    }
+    
     @Test
     public void itCalculatesTheDistanceFromAnotherCoordinateAlongXAxis() {
         Coordinate a = new Coordinate(0, 0);
