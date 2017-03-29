7950a662a3948282ab66e8ed435c371bbc881455
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-03-29 14:55:06 +0100

    Improve legibility of JUnit test

--- src/test/java/shouty/CoordinateTest.java
+++ src/test/java/shouty/CoordinateTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 public class CoordinateTest {
 
     @Test
-    public void itCalculatesTheDistanceFromAnotherCoordinateAlongXAxis() {
+    public void it_calculates_the_distance_from_another_coordinate_along_x_axis() {
         Coordinate a = new Coordinate(0, 0);
         Coordinate b = new Coordinate(1000, 0);
         assertEquals(1000, a.distanceFrom(b));
