582e590f5ff69cc06db869472934407a4bedb562
Seb Rose <github@claysnow.co.uk>
2017-04-15 12:32:31 +0100

    Add missing unit test
    This now conforms with the workbook that talks about 2 unit tests (1 passing, 1 failing) and 2 scenarios (1 passing, 1 failing) on page 2

--- src/test/java/shouty/CoordinateTest.java
+++ src/test/java/shouty/CoordinateTest.java
@@ -4,6 +4,13 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CoordinateTest {
+    
+    @Test
+    public void itCalculatesTheDistanceFromItself() {
+        Coordinate a = new Coordinate(0, 0);
+        assertEquals(0, a.distanceFrom(a));
+    }
+
 
     @Test
     public void it_calculates_the_distance_from_another_coordinate_along_x_axis() {
