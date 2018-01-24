4815aebc708545382979d6361a38068dfab53e84
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-01-24 12:00:16 +0000

    Add JSON formatter

--- src/test/java/shouty/RunCukesTest.java
+++ src/test/java/shouty/RunCukesTest.java
@@ -6,7 +6,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(monochrome = true, plugin = {"pretty", "html:target/cucumber", "rerun:target/rerun.txt", "io.cucumber.pro.JsonReporter"}, snippets = SnippetType.CAMELCASE)
+@CucumberOptions(monochrome = true, plugin = {"pretty", "html:target/cucumber", "rerun:target/rerun.txt", "json:target/cucumber.json", "io.cucumber.pro.JsonReporter"}, snippets = SnippetType.CAMELCASE)
 public class RunCukesTest {
   // this is the adapter/bridge code
   // between cucumber jvm and junit
