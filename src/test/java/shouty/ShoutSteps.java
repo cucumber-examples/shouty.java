e8b7a946bd75623c98357209652a462613d47a0f
Thomas Sundberg <tsu@kth.se>
2018-05-08 10:23:44 +0200

    Removed a duplicated step

--- src/test/java/shouty/ShoutSteps.java
+++ src/test/java/shouty/ShoutSteps.java
@@ -4,7 +4,6 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-import static java.util.Collections.emptyList;
 import static java.util.Collections.emptyMap;
 import static org.junit.Assert.assertEquals;
 
@@ -13,28 +12,23 @@ public class ShoutSteps {
     private static final String ARBITRARY_MESSAGE = "Hello, world";
     private final Shouty shouty = new Shouty();
 
-    @Given("^Lucy is at (\\d+), (\\d+)$")
-    public void lucy_is_at(int xCoord, int yCoord) throws Throwable {
-        shouty.setLocation("Lucy", new Coordinate(xCoord, yCoord));
-    }
-
-    @Given("^Sean is at (\\d+), (\\d+)$")
-    public void sean_is_at(int xCoord, int yCoord) throws Throwable {
-        shouty.setLocation("Sean", new Coordinate(xCoord, yCoord));
+    @Given("^(Lucy|Sean) is at (\\d+), (\\d+)$")
+    public void lucy_is_at(String name, int xCoord, int yCoord) {
+        shouty.setLocation(name, new Coordinate(xCoord, yCoord));
     }
 
     @When("^Sean shouts$")
-    public void sean_shouts() throws Throwable {
+    public void sean_shouts() {
         shouty.shout("Sean", ARBITRARY_MESSAGE);
     }
 
     @Then("^Lucy should hear Sean")
-    public void lucy_should_hear_sean() throws Throwable {
+    public void lucy_should_hear_sean() {
         assertEquals(1, shouty.getShoutsHeardBy("Lucy").size());
     }
 
     @Then("^Lucy should hear nothing$")
-    public void lucy_should_hear_nothing() throws Throwable {
+    public void lucy_should_hear_nothing() {
         assertEquals(emptyMap(), shouty.getShoutsHeardBy("Lucy"));
     }
 }
