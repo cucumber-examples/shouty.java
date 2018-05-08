ca0f71cec4575e737d721b9b2afd55e8eb119f0d
Thomas Sundberg <tsu@kth.se>
2018-05-08 09:59:25 +0200

    Calculate the distance between two coordinates along the x-axis

--- src/main/java/shouty/Coordinate.java
+++ src/main/java/shouty/Coordinate.java
@@ -1,18 +1,15 @@
 package shouty;
 
-public class Coordinate {
+class Coordinate {
     private final int x;
     private final int y;
 
-    public Coordinate(int xCoord, int yCoord) {
+    Coordinate(int xCoord, int yCoord) {
         x = xCoord;
         y = yCoord;
     }
 
-    public int distanceFrom(Coordinate other) {
-        // TODO: actually calculate distance between the coordinates.
-        //       e.g. return Math.abs(x - other.x);
-
-        return 0;
+    int distanceFrom(Coordinate other) {
+        return Math.abs(x - other.x);
     }
 }
