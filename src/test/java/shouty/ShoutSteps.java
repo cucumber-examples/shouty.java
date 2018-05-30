c0833c8a36b43c8f3ab21a0a65a3ee2b2c946a9d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-05-30 09:51:21 +0100

    Add shim for types. Upgrade datatable.

--- src/test/java/shouty/ShoutSteps.java
+++ src/test/java/shouty/ShoutSteps.java
@@ -4,7 +4,6 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-import static java.util.Collections.emptyList;
 import static java.util.Collections.emptyMap;
 import static org.junit.Assert.assertEquals;
 
