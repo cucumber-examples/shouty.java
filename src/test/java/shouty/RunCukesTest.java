404bd96a44386d3c4c730721b1f936d9cd245821
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2020-03-04 13:08:21 +0000

    Update to cucumber-jvm 5.4.0

--- src/test/java/shouty/RunCukesTest.java
+++ src/test/java/shouty/RunCukesTest.java
@@ -6,7 +6,16 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(monochrome = true, plugin = {"pretty", "html:target/cucumber", "rerun:target/rerun.txt", "json:target/cucumber.json"}, snippets = SnippetType.CAMELCASE)
+@CucumberOptions(
+        monochrome = true,
+        plugin = {
+                "pretty",
+                "html:target/cucumber",
+                "rerun:target/rerun.txt",
+                "json:target/cucumber.json"
+        },
+        snippets = SnippetType.CAMELCASE
+)
 public class RunCukesTest {
   // this is the adapter/bridge code
   // between cucumber jvm and junit
--- src/test/java/shouty/ShoutyTypes.java
+++ src/test/java/shouty/ShoutyTypes.java
@@ -1,23 +1,4 @@
 package shouty;
 
-import io.cucumber.core.api.TypeRegistry;
-import io.cucumber.cucumberexpressions.CaptureGroupTransformer;
-import io.cucumber.cucumberexpressions.ParameterType;
-import io.cucumber.datatable.DataTableType;
-import io.cucumber.datatable.TableEntryTransformer;
-
-import java.util.Locale;
-
-import static java.lang.Integer.parseInt;
-
-public class ShoutyTypes implements io.cucumber.core.api.TypeRegistryConfigurer {
-    @Override
-    public Locale locale() {
-        return Locale.ENGLISH;
-    }
-
-    @Override
-    public void configureTypeRegistry(TypeRegistry typeRegistry) {
-    }
-
+public class ShoutyTypes {
 }
